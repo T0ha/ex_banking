805d4d9b33852eb7a2e4dc19419e027ddb3f40b1
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-02-17 11:04:13 +0500

    Added parebones for the interface functions

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,18 +1,53 @@
 defmodule ExBanking do
   @moduledoc """
-  Documentation for `ExBanking`.
+  Simple banking application
   """
+
+  @type user() :: String.t()
+  @type currency() :: String.t()
+  @type balance() :: float()
+  @type amount() :: float()
+
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
-
+  Function creates new user in the system
+  with 0.0 balance for any currency
   """
-  def hello do
-    :world
+  @spec create_user(user()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user), do: :ok
+
+  @doc """
+  Returns new_balance of the user in given format
+  """
+  @spec deposit(user(), amount(), currency()) :: {:ok, balance()} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(user, amount, currency) do
+    {:ok, amount}
+  end
+
+
+  @doc """
+  Decreases user’s balance in given currency by amount value
+  Returns new_balance of the user in given format
+  """
+  @spec withdraw(user(), amount(), currency()) :: {:ok, balance()} | {:error, :wrong_arguments | :user_does_not_exist | :not_enough_money | :too_many_requests_to_user}
+  def withdraw(user, amount, currency) do
+    {:ok, amount}
+  end
+
+
+  @doc """
+  Returns balance of the user in given format
+  """
+  @spec get_balance(user(), currency()) :: {:ok, balance()} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def get_balance(user, currency) do
+    {:ok, 0.0}
+  end
+
+
+  @doc """
+  """
+  @spec send(user(), user(), amount(), currency()) :: {:ok, balance(), balance()} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
+  def send(from_user, to_user, amount, currency) do
+    {:ok, 0.0, amount}
   end
 end
