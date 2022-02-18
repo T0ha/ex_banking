ee79c4c7c03604fd0dd6dbda7e58ce4d1e16d366
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-02-18 13:24:11 +0500

    Added dialyzer

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule ExBanking.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:faker, "~> 0.17", only: :test}
+      {:faker, "~> 0.17", only: :test},
+      {:dialyxir, "~> 1.1", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
